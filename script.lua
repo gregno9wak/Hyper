257ad236f33655c081828d29190cc8d49b3691e0
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2023-01-31 16:58:12 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruit
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/bf.lua"))()
+elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 then
+    --King Legacy
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
+elseif placeId == 9380307595 then
+    --Pixel Piece
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/pp.lua"))()
+end
