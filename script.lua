01912190fbc169a583b9f109ea18e2599c1a2917
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2023-02-11 09:18:43 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,7 +4,7 @@ if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/bf.lua"))()
 elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 then
     --King Legacy
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kll.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
 elseif placeId == 9380307595 then
     --Pixel Piece
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/pp.lua"))()
